668d9b79f376bffdd1fa79a64eab6cd956cdc632
Dekel Maman <dekel.maman@autodesk.com>
2024-01-23 17:46:30 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/pointfreeco/swift-composable-architecture",
-      .upToNextMinor(from: "1.5.5"))
+      .upToNextMinor(from: "0.57.0"))
   ],
   targets: [
     .target(
